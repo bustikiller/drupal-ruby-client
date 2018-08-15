7b6a88424a3de93dce4be75fb1e86a68124f4b4c
Manuel Bustillo <mayn13@gmail.com>
2018-08-15 13:47:09 +0200

    Remove field processing in nodes

--- node.rb
+++ node.rb
@@ -1,34 +1,19 @@
-require_relative 'parser'
-
 class Node
 
-    FIELD_PREFIX = 'field_'
-
+    attr_reader :nid, :type, :title, :raw
     def initialize(params)
         @nid = params['nid']
         @type = params['type']
         @title = params['title']
         @deep_load = false
+        @raw = {}
     end
 
     def deep_load!
         if !@deep_load
-            params = Client.instance.deep_load(nid: @nid)
-            fields = params.select{|k, _| k.start_with? FIELD_PREFIX}
-
-            fields.each{|k, v| process_field(k, v)}
+            @raw = Client.instance.deep_load(nid: @nid)
             @deep_load = true
         end
         return self
-    end
-
-    private
-
-    def process_field(key, value)
-        field_name = key.sub FIELD_PREFIX, ''
-        field_value = Parsers::Parser.parse(value)
-
-        instance_variable_set "@#{field_name}", field_value
-        self.class.attr_accessor field_name.to_sym
     end
 end
